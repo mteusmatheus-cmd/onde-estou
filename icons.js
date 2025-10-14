44f7cab204d3d8af26b8f25bccbaa39f9e33cb27
mteusmatheus-cmd <mteus.matheus@gmail.com>
2025-10-13 22:18:19 -0300

    Update icons.js

--- icons.js
+++ icons.js
@@ -156,6 +156,39 @@ const Icons = {
     React.createElement('path', { d: 'M7 4h.01' }),
     React.createElement('path', { d: 'M12 4h.01' }),
     React.createElement('path', { d: 'M17 4h.01' })
+  ),
+
+  Trash2: (props) => React.createElement('svg', {
+    xmlns: 'http://www.w3.org/2000/svg',
+    width: props.size || 24,
+    height: props.size || 24,
+    viewBox: '0 0 24 24',
+    fill: 'none',
+    stroke: 'currentColor',
+    strokeWidth: 2,
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+    className: props.className
+  },
+    React.createElement('polyline', { points: '3 6 5 6 21 6' }),
+    React.createElement('path', { d: 'M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2' }),
+    React.createElement('line', { x1: '10', y1: '11', x2: '10', y2: '17' }),
+    React.createElement('line', { x1: '14', y1: '11', x2: '14', y2: '17' })
+  ),
+
+  ThumbsUp: (props) => React.createElement('svg', {
+    xmlns: 'http://www.w3.org/2000/svg',
+    width: props.size || 24,
+    height: props.size || 24,
+    viewBox: '0 0 24 24',
+    fill: 'none',
+    stroke: 'currentColor',
+    strokeWidth: 2,
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+    className: props.className
+  },
+    React.createElement('path', { d: 'M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3' })
   )
 };
 
