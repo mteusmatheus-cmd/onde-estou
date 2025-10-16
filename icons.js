b81c20c0b592b7bff8da8e6c89abd78d8dc2edab
mteusmatheus-cmd <mteus.matheus@gmail.com>
2025-10-16 08:26:23 -0300

    Update icons.js

--- icons.js
+++ icons.js
@@ -189,6 +189,22 @@ const Icons = {
     className: props.className
   },
     React.createElement('path', { d: 'M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3' })
+  ),
+
+  StickyNote: (props) => React.createElement('svg', {
+    xmlns: 'http://www.w3.org/2000/svg',
+    width: props.size || 24,
+    height: props.size || 24,
+    viewBox: '0 0 24 24',
+    fill: 'none',
+    stroke: 'currentColor',
+    strokeWidth: 2,
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+    className: props.className
+  },
+    React.createElement('path', { d: 'M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z' }),
+    React.createElement('path', { d: 'M14 2v4a2 2 0 0 0 2 2h4' })
   )
 };
 
